865d7bb4d79b7754df198cf0098ee926c1eb28c7
Raees-Khan05 <154559811+Raees-Khan05@users.noreply.github.com>
2024-04-17 02:07:30 +0500

    little work of chapter from 21 to 25

---
+++ chapter-21 to 25/app.js
@@ -0,0 +1,151 @@
+
+
+
+
+// var txt = 'Which platform you will be using for the payment.'
+
+// // if(abc == ' ')
+// for (var i = 0; i < txt.length; i++) {
+//     if (txt.slice(i, i + 7) === 'payment') {
+//         alert('Payment word is not allowed')
+//     }
+// }   
+
+
+
+
+// task no 01 
+
+var userFirstName = prompt("enter your first name")
+
+var userLastName = prompt("enter your last name")
+
+var fullName = userFirstName + " " + userLastName
+
+alert("hello " + fullName + "....")
+
+
+
+
+
+// task no 02 
+
+// Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+
+var usersInput = prompt("enter your fav mob model name")
+
+
+alert("length of string is : " + usersInput.length)
+
+
+
+// Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+
+
+
+
+          var name = "Pakistani"
+
+         var index = -1
+
+          for (let i = 0; i < name.length; i++) {
+            if (name[i] === "n") {
+                index = i
+                break
+            }
+            
+          }
+
+          if (index !== -1) {
+            console.log("The index of","n", "is:", index);
+        } else {
+            console.log("n", "is not in the array.");
+        }
+
+
+
+  // question no 4 , 5 , 6 , 7 , 8 , 9 are remaining
+  
+  
+
+  // question no 07
+
+
+  //Page 3 of 9
+// 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+
+
+// var word = 'hyderabad'
+
+
+
+
+
+
+
+   // question no 10
+//  Write a program that takes user input. Convert and
+// show the input in capital letters.
+
+
+
+
+var user_Input = "peanuts"
+
+console.log(user_Input.toUpperCase())
+
+
+
+// task no 11
+// Write a program that takes user input. Convert and
+// show the input in title case.
+
+
+ var userLang = 'javascript'
+
+ console.log(userLang.slice(0))
+
+ console.log(userLang[0].toUpperCase() + userLang.slice(1))
+
+
+
+
+
+
+
+ // task no 17
+
+
+ // Write a program to display the last character of a user input.
+
+
+   var country = "Pakistan"
+
+   console.log("last character of input is: " +  country[7])
+
+//    for (let i = 0; i < country.length; i++) {
+        
+//     console.log(country[7])
+    
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
